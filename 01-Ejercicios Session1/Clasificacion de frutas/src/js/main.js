c844776001354d3c6ce1ebbbe010c5c1b459fa0e
Karina P <karina_pineda97@hotmail.com>
2024-04-15 13:41:51 -0500

    se inicia el proyecto de seleccion de frutas y se crean los arreglos para diferenciarlas

---
+++ 01-Ejercicios Session1/Clasificacion de frutas/src/js/main.js
@@ -0,0 +1,8 @@
+let fruits=["Manzana","Mandarina","Pera","Mora","Fresa", "Banana","Naranja","Limon"]
+
+fruits.forEach(fruits => {
+    console.log(fruits)
+});
+
+let citrusFruits=[]
+let sweetFruits=[]
