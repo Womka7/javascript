9e5cc7e58f4fae4884ef31bc5dfb60a5ea750b78
Karina P <karina_pineda97@hotmail.com>
2024-04-16 10:56:39 -0500

    informacion sobre manejo de strings

---
+++ 02-MANEJODESTRINGS/src/js/main.js
@@ -0,0 +1,24 @@
+// let ejemplo = "Hola mundo"
+
+
+// ---------------------DECLARACION DE STRINGS -------------------------
+// const option1 = "Hola mundo"
+// const option2 = String("Hola mundo")
+// const option3 = new String("Hola mundo")
+
+// console.info(option1)
+// console.info(option2)
+// console.info(option3)
+
+// ----------------------EXTRACCION DE DATOS---------------------------
+const message="Este es un mensaje para pruebas"
+document.write(message)
+
+// ------------------EXTRAER LA CANTIDAD DE CARACTERES-----------------
+console.info(message.length)
+
+// ------------------------EXTRAER UN CARACTER---------------------
+console.info(message[1])
+console.info(message.charAt(1))
+console.info(message.search("mensaje"))
+console.info(message.includes("mensaje"))
