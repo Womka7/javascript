373c9ebf31348367ace14166c39712babf411003
Karina P <karina_pineda97@hotmail.com>
2024-04-16 13:42:50 -0500

    se inciaron los ejercicios

---
+++ 02-Ejercicios Session2/src/js/main.js
@@ -0,0 +1,17 @@
+// 1. Declara dos variables, num1 y num2, asignándoles valores 
+// numéricos diferentes y muestra en consola el resultado de sumarlos.
+
+let num1 = 1
+let num2 = 5
+suma = num1 + num2
+console.info(suma)
+
+// 2. Define dos variables de tipo string, simpleString1 y simpleString2,
+//  con valores "Hola" y "Mundo" respectivamente. Muestra en consola 
+//  la longitud total de ambas cadenas.
+
+let simpleString1="Hola"
+let simpleString2="Mundo"
+
+console.info(simpleString1.length)
+console.info(simpleString2.length)
