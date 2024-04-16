fbb3dd1f019e6affcd6439624d2ce1d8009bec92
Karina P <karina_pineda97@hotmail.com>
2024-04-16 07:11:00 -0500

    diferente manera de un for

--- 01-Ejercicios Session1/Clasificacion de frutas/src/js/main.js
+++ 01-Ejercicios Session1/Clasificacion de frutas/src/js/main.js
@@ -1,8 +1,14 @@
 let fruits=["Manzana","Mandarina","Pera","Mora","Fresa", "Banana","Naranja","Limon"]
 
+
+let citrusFruits=[]
+let sweetFruits=[]
+
+for (i=0; i< fruits.length; i++){
+    console.table(fruits[i])
+}
+
 fruits.forEach(fruits => {
     console.log(fruits)
 });
 
-let citrusFruits=[]
-let sweetFruits=[]
