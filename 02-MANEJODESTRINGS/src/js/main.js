e52b5c70d0bd59312eba8d6ec85976c448128c62
Karina P <karina_pineda97@hotmail.com>
2024-04-16 12:37:18 -0500

    ejemplos de concatenacion y limpieza de espacios

--- 02-MANEJODESTRINGS/src/js/main.js
+++ 02-MANEJODESTRINGS/src/js/main.js
@@ -11,17 +11,17 @@
 // console.info(option3)
 
 // ----------------------EXTRACCION DE DATOS---------------------------
-const message="Este es un mensaje para pruebas"
-document.write(message)
+// const message="Este es un mensaje para pruebas"
+// document.write(message)
 
 // ------------------EXTRAER LA CANTIDAD DE CARACTERES-----------------
-console.info(message.length)
+// console.info(message.length)
 
 // ------------------------EXTRAER UN CARACTER---------------------
-console.info(message[1])
-console.info(message.charAt(1))
-console.info(message.search("mensaje"))
-console.info(message.includes("mensaje"))
+// console.info(message[1])
+// console.info(message.charAt(1))
+// console.info(message.search("mensaje"))
+// console.info(message.includes("mensaje"))
 
 // ------------------------INTERPOLACION CON CADENAS, CONCATENACIOM------------------------
 const messageOne="Este es un mensaje para"
@@ -34,10 +34,30 @@ console.info(messageTwo)
 console.info("option 1(+):"+messageOne+" "+messageTwo)
 console.info("option 2(,):",messageOne,messageTwo)
 console.info(`option 3(\`): ${messageOne}${messageTwo}`)
+console.info("option 4 concat() simple", messageOne.concat(" ").concat(messageTwo))
+console.info("option 4 concat() abreviada", messageOne.concat(" ",messageTwo))
+
 
 // -----------------------------INTERPOLACION CON COMPONENTE-------------
-const header=`
-<header>
-    <h1>Mi nombre es ${fullName}</h1>
-</header>`
-document.write(header)
+// const header=`
+// <header>
+//     <h1>Mi nombre es ${fullName}</h1>
+// </header>`
+// document.write(header)
+
+// --------------------DEPURACION DE STRINGS------------------
+// const message="HolA coMO eSTas"
+// document.write(message)
+
+// -----------------transformacion a minuscula----------------------
+// console.info(message.toLowerCase())
+// -----------------transformacion a mayuscula----------------------
+// console.info(message.toUpperCase())
+
+// --------------------LIMPIEZA DE ESPACIOS-----------------------
+// const messageOne="     espruebaemail@gmail.com    "
+
+// console.info(messageOne)
+// console.info(messageOne.trimStart())
+// console.info(messageOne.trimEnd())
+// console.info(messageOne.trim())
