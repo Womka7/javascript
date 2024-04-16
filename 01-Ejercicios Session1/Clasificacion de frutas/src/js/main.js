f537cd5cc484bbc1d175dea7c50b1859c9903b0b
Karina P <karina_pineda97@hotmail.com>
2024-04-16 07:38:17 -0500

    Ejercicio e las frutas finalizado con clasificacion

--- 01-Ejercicios Session1/Clasificacion de frutas/src/js/main.js
+++ 01-Ejercicios Session1/Clasificacion de frutas/src/js/main.js
@@ -4,11 +4,35 @@ let fruits=["Manzana","Mandarina","Pera","Mora","Fresa", "Banana","Naranja","Lim
 let citrusFruits=[]
 let sweetFruits=[]
 
-for (i=0; i< fruits.length; i++){
-    console.table(fruits[i])
+for ( let i=0; i< fruits.length; i++){
+    console.log(fruits[i])
 }
 
-fruits.forEach(fruits => {
-    console.log(fruits)
-});
+// fruits.forEach(fruits => {
+//     console.log(fruits)
+// });
 
+for (let i=0; i< fruits.length; i++){
+    if (fruits[i]=="Manzana"||fruits[i]== "Mora"||fruits[i]== "Fresa"||fruits[i]== "Banana"||fruits[i]== "Pera"){
+        sweetFruits.push(fruits[i])
+    }else{
+        citrusFruits.push(fruits[i])
+    }
+}
+
+// fruits.forEach(fruits => {
+//      if (fruits=="Manzana"||fruits== "Mora"||fruits== "Fresa"||fruits== "Banana"||fruits== "Pera"){
+//         sweetFruits.push(fruits)
+//     }else{
+//         citrusFruits.push(fruits)
+//     }
+// });   
+
+
+console.group("Frutas critricas")
+console.table(citrusFruits)
+console.groupEnd
+
+console.group("Frutas dulces")
+console.table(sweetFruits)
+console.groupEnd
