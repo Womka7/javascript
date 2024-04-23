195517c09e472bbb8afc96f90b8ca1122ff965a7
Karina P <karina_pineda97@hotmail.com>
2024-04-23 07:42:07 -0500

    creacion de carpetaa de ejercicion sesion 6 sobre estructuras de control junto a 1 ejercicio 1 compeltado

---
+++ 06-Ejercicios Session6/src/js/main.js
@@ -0,0 +1,64 @@
+// Ejercicio 1
+console.groupCollapsed("Ejercicio1")
+let number1=Number(prompt("Ingresa un numero"))
+let number2=Number(prompt("Ingresa un numero"))
+
+if(number1<number2){
+    console.log(`${number1} es menor que ${number2}`)
+}else if(number1 ==number2){
+    console.log(`${number1} y ${number2} son iguales`)
+}else if(number1>number2){
+    let resta = (number2-number1)
+    let estador=resta.toString()
+    if(estador.includes("-")){
+        console.log(`${estador} es un numero negativo`)
+    }   
+}else{
+    console.log(`No son datos numericos`)
+}
+console.groupEnd()
+
+// Ejercicio 2
+console.groupCollapsed("Ejercicio2")
+
+console.groupEnd()
+
+// Ejercicio 3
+console.groupCollapsed("Ejercicio3")
+
+console.groupEnd()
+
+// Ejercicio 4
+console.groupCollapsed("Ejercicio4")
+
+console.groupEnd()
+
+// Ejercicio 5
+console.groupCollapsed("Ejercicio5")
+
+console.groupEnd()
+
+// Ejercicio 6
+console.groupCollapsed("Ejercicio6")
+
+console.groupEnd()
+
+// Ejercicio 7
+console.groupCollapsed("Ejercicio7")
+
+console.groupEnd()
+
+// Ejercicio 8
+console.groupCollapsed("Ejercicio8")
+
+console.groupEnd()
+
+// Ejercicio 9
+console.groupCollapsed("Ejercicio9")
+
+console.groupEnd()
+
+// Ejercicio 10
+console.groupCollapsed("Ejercicio10")
+
+console.groupEnd()
